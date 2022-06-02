d098f8ed2467ef99d1462f660d7811e78f1d6278
WafiqAli <wafiqcali@gmail.com>
2022-06-02 23:39:55 +1200

    Connected backend index.js to mongoDB and created GET request to retrieve product list
    Used mongoose to connect to mongoDB, then created the appropriate schema + model to run find() command and retrieve collection of Products. Then made an endpoint to GET list of products.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,38 @@
 const  express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const { Admin } = require("mongodb");
 
 dotenv.config();
-
 const server = express();
 server.use(express.json());
-
 const PORT = process.env.PORT || 9999;
+const url = 'mongodb://admin:admin@mongo:27017/petshop-db?authSource=admin';
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('MongoDB Connected'))
+        .catch(err => console.log(err));
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const productsSchema = new mongoose.Schema(
+    {
+        productName: String,
+        productDescription: String,
+        quantity: Number
+    }, 
+    { collection: 'Products' }
+);
+const Products = mongoose.model('Products', productsSchema);
+
 
 server.listen(PORT, function() {
     console.log(`listening to port ${PORT}`);
+});
+
+server.get('/products', (req, res) => {
+
+    Products.find().then(result => res.send(result)).catch(err => console.log(err));
+
 });
