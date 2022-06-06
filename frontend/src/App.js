3f0b3623e82f318ba15242455f426c0a44374ae2
WafiqAli <wafiqcali@gmail.com>
2022-06-06 20:23:23 +1200

    Added image assets and code clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,11 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import SortedProductNames from './components/SortedProductNames';
+
 
 function App() {
-
-  const [products, setProducts] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:9001/products')
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw response;
-      })
-      .then(data => {
-        setProducts(data);
-        console.log(data);
-      })
-      .catch(err => console.log(err));
-    }, []);
-  
   
   return (
     <div className="App">
-      <SortedProductNames products={products}/>
+    
     </div>
   );
 }
