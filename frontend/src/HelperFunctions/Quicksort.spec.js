f595d92a72c628efe64a26cfebd4264dbc31c58a
WafiqAli <wafiqcali@gmail.com>
2022-06-10 04:40:43 +1200

    added Jest and created .spec.js to unit test Quicksort function

---
+++ frontend/src/HelperFunctions/Quicksort.spec.js
@@ -0,0 +1,41 @@
+const Quicksort = require("./Quicksort.js");
+
+
+describe("Quicksort function Tests", () => {
+
+    const testCases = [
+        {
+            input: ["apple", "bone", "crust", "dragon", "Eagle"], //already sorted
+            output: ["apple", "bone", "crust", "dragon", "Eagle"]
+        },
+        {
+            input: ["crust", "apple", "Eagle", "dragon", "bone"], // unsorted normal
+            output: ["apple", "bone", "crust", "dragon", "Eagle"]
+        },
+        {
+            input: ["standalone"], //one element 
+            output: ["standalone"]
+        },
+        {
+            input: ["Frog", "Deer"], //two elements
+            output: ["Deer", "Frog"]
+        },
+        {
+            input: ["astronomy", "apple","awesome", "abnormal", "advertisement"], //all elements with same first letter
+            output: ["abnormal", "advertisement", "apple", "astronomy", "awesome"]
+        },
+        {
+            input: [], //empty list
+            output: []
+        },
+    ];
+
+
+    testCases.map(tc => (
+        it(`returns ${tc.output} when ${tc.input}`, () => {
+            const actual = Quicksort(tc.input);
+            expect(actual).toStrictEqual(tc.output);
+        })
+    ))
+
+})
